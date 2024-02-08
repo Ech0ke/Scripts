7be1aeb7d834e50a2463a2b65d016f78f05d030c
Ech0ke <aidas.zygas@gmail.com>
2024-02-08 09:01:27 +0200

    Output file names and paths of specified type and directory into .txt file

--- batch/main.bat
+++ batch/main.bat
@@ -30,57 +30,19 @@ rem Remove leading comma from arrays
 set "fileNames=!fileNames:~1!"
 set "filePaths=!filePaths:~1!"
 
-rem Print the arrays (optional)
+rem Print the arrays
 echo File Names: !fileNames!
 echo File Paths: !filePaths!
 
+rem Get current date and time
+set "currentDateTime=%DATE% %TIME%"
+
+rem Output arrays to a log file
+(
+    echo !currentDateTime!
+    echo.
+    echo File Names: !fileNames!
+    echo File Paths: !filePaths!
+) > "%directory%\summary.log"
+
 exit /b
-
-@REM rem Initialize the two-dimensional array
-@REM set "row=0"
-@REM set "col=0"
-
-@REM rem Define a subroutine to process files in the directory and its subdirectories
-@REM :ProcessFiles
-@REM rem Reset the column index before processing files in the current directory
-@REM set "col=0"
-@REM for /f "delims=" %%F in ('dir /b /s "%~1\*.%extension%" 2^>nul') do (
-@REM     set "files[!row!][!col!]=%%~nxF"
-@REM     set "dirs[!row!][!col!]=%%~dpF"
-@REM     set /a col+=1
-@REM )
-@REM for /d %%D in ("%~1\*") do (
-@REM     call :ProcessFiles "%%D"
-@REM )
-@REM set /a row+=1
-
-@REM rem Print the table header
-@REM echo Filename^|Filepath
-
-@REM rem Loop through the array and print the contents as a table
-@REM for /l %%r in (0,1,%row%) do (
-@REM     for /l %%c in (0,1,!col!) do (
-@REM         if defined files[%%r][%%c] (
-@REM             echo !files[%%r][%%c]!^|!dirs[%%r][%%c]!
-@REM         )
-@REM     )
-@REM )
-
-
-
-@REM rem Accumulate found files into an array
-@REM set "index=0"
-@REM for /f "delims=" %%F in ('dir /b /s "%directory%\*.%extension%" 2^>nul') do (
-@REM     set "file[!index!]=%%F"
-@REM     set /a index+=1
-@REM )
-
-@REM rem Loop through the array and print file names with paths
-@REM if %index% equ 0 (
-@REM     echo No files with extension %extension% found in %directory% or its subdirectories.
-@REM ) else (
-@REM     echo Files with extension %extension% found in %directory% or its subdirectories:
-@REM     for /l %%i in (0,1,%index%) do (
-@REM         echo !file[%%i]!
-@REM     )
-@REM )
